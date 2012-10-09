f0d844bf754e9cecfd83e55aa166b2724e9c6219
Salvatore D'Agositno <toto19@gmail.com>
2012-10-08 20:52:23 -0400

    added route for about page

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,10 @@
 
 exports.index = function(req, res){
     console.log('Index Requested');
-  res.render('index', { title: 'Red Ship Studios' });
+    res.render('index', { title: 'Red Ship Studios' });
+};
+
+exports.about = function(req,res){
+    console.log('About Requested');
+    res.render('about',{ title: 'Red Ship Studios - About'});
 };
